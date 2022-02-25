757300a7ca95f22084fe4e8c5b5af13ffa52f80f
phamquynhtan <phamtanquynh1709@gmail.com>
2022-02-24 23:08:13 -0500

    update 5 day forecast

--- app/src/main/java/com/raiders/theweatherology/SecondFragment.kt
+++ app/src/main/java/com/raiders/theweatherology/SecondFragment.kt
@@ -31,9 +31,39 @@ class SecondFragment : Fragment() {
 
         val tempObserver = Observer<Double>{temp-> textTemp.text = ("%.0f"+ "\u2103").format(temp)}
         viewModel.getTemp().observe(viewLifecycleOwner,tempObserver)
+        val tempObserver1 = Observer<Double>{temp1-> textTempOne.text="%.0f °F".format(temp1)}
+        viewModel.getTemp1().observe(viewLifecycleOwner,tempObserver1)
+
+        val tempObserver2 = Observer<Double>{temp2-> textTempTwo.text="%.0f °F".format(temp2)}
+        viewModel.getTemp2().observe(viewLifecycleOwner,tempObserver2)
+
+        val tempObserver3 = Observer<Double>{temp3-> textTempThree.text="%.0f °F".format(temp3)}
+        viewModel.getTemp3().observe(viewLifecycleOwner,tempObserver3)
+
+        val tempObserver4 = Observer<Double>{temp4-> textTempFour.text="%.0f °F".format(temp4)}
+        viewModel.getTemp4().observe(viewLifecycleOwner,tempObserver4)
+
+        val tempObserver5 = Observer<Double>{temp5-> textTempFive.text="%.0f °F".format(temp5)}
+        viewModel.getTemp5().observe(viewLifecycleOwner,tempObserver5)
+
 
         val dateObserver = Observer<String> {date-> textDate.text = date.toString()}
         viewModel.getDate().observe(viewLifecycleOwner,dateObserver)
+        val dateObserver1= Observer<String> {date1->dayOne.text=date1.toString()}
+        viewModel.getDate1().observe(viewLifecycleOwner,dateObserver1)
+
+        val dateObserver2= Observer<String> {date2->dayTwo.text=date2.toString()}
+        viewModel.getDate2().observe(viewLifecycleOwner,dateObserver2)
+
+        val dateObserver3= Observer<String> {date3->dayThree.text=date3.toString()}
+        viewModel.getDate3().observe(viewLifecycleOwner,dateObserver3)
+
+        val dateObserver4= Observer<String> {date4->dayFour.text=date4.toString()}
+        viewModel.getDate4().observe(viewLifecycleOwner,dateObserver4)
+
+        val dateObserver5= Observer<String> {date5->dayFive.text=date5.toString()}
+        viewModel.getDate5().observe(viewLifecycleOwner,dateObserver5)
+
 
         val windObserver = Observer<Double> {wind-> TextWind.text = ("%.2f"+ " m/s").format(wind)}
         viewModel.getWind().observe(viewLifecycleOwner,windObserver)
@@ -46,13 +76,13 @@ class SecondFragment : Fragment() {
         {feelsLike-> textFeelsLike.text = ("%.0f"+ "\u2103").format(feelsLike)}
         viewModel.getFeelsLike().observe(viewLifecycleOwner,feelsLikeObserver)
 
-//        val minTempObserver = Observer<Double>
-//        {minTemp-> textMinTemp.text = ("%.0f"+ "\u2103").format(minTemp)}
-//        viewModel.getMinTemp().observe(viewLifecycleOwner,minTempObserver)
-//
-//        val maxTempObserver = Observer<Double>
-//        {maxTemp-> textMaxTemp.text = ("%.0f"+ "\u2103").format(maxTemp)}
-//        viewModel.getMaxTemp().observe(viewLifecycleOwner,maxTempObserver)
+        val minTempObserver = Observer<Double>
+        {minTemp-> minTem.text = ("%.0f"+ "\u2103").format(minTemp)}
+        viewModel.getMinTemp().observe(viewLifecycleOwner,minTempObserver)
+
+        val maxTempObserver = Observer<Double>
+        {maxTemp-> maxTem.text = ("%.0f"+ "\u2103").format(maxTemp)}
+        viewModel.getMaxTemp().observe(viewLifecycleOwner,maxTempObserver)
 
         val mainDescriptionObserver = Observer<String>
         {mainDescription-> textMainDecription.text = mainDescription.toString()}
@@ -62,6 +92,8 @@ class SecondFragment : Fragment() {
         textCity.text = arguments?.getString("message")
         val queue = Volley.newRequestQueue(context)
         arguments?.getString("message")?.let { viewModel.details(it, queue) }
+        val queue2 = Volley.newRequestQueue(context)
+        arguments?.getString("message")?.let { viewModel.forecast(it, queue2) }
 
     }
 
--- app/src/main/java/com/raiders/theweatherology/SecondViewModel.kt
+++ app/src/main/java/com/raiders/theweatherology/SecondViewModel.kt
@@ -18,13 +18,56 @@ class SecondViewModel : ViewModel() {
     private var wind: MutableLiveData<Double> = MutableLiveData()
     private var humidity:MutableLiveData<Int> = MutableLiveData()
     private var feelsLike:MutableLiveData<Double> = MutableLiveData()
-    //    private var minTemp:MutableLiveData<Double> = MutableLiveData()
-//    private var maxTemp:MutableLiveData<Double> = MutableLiveData()
+        private var minTemp:MutableLiveData<Double> = MutableLiveData()
+    private var maxTemp:MutableLiveData<Double> = MutableLiveData()
     private var mainDescription:MutableLiveData<String> = MutableLiveData()
+    private var temp1: MutableLiveData<Double> = MutableLiveData()
+    private var temp2: MutableLiveData<Double> = MutableLiveData()
+    private var temp3: MutableLiveData<Double> = MutableLiveData()
+    private var temp4: MutableLiveData<Double> = MutableLiveData()
+    private var temp5: MutableLiveData<Double> = MutableLiveData()
+    private var date1: MutableLiveData<String> = MutableLiveData()
+    private var date2: MutableLiveData<String> = MutableLiveData()
+    private var date3: MutableLiveData<String> = MutableLiveData()
+    private var date4: MutableLiveData<String> = MutableLiveData()
+    private var date5: MutableLiveData<String> = MutableLiveData()
+
 
     //Getters
     fun getTemp():MutableLiveData<Double>{
         return temp
+    }
+    fun getTemp1():MutableLiveData<Double>{
+        return temp1
+    }
+    fun getTemp2():MutableLiveData<Double>{
+        return temp2
+    }
+    fun getTemp3():MutableLiveData<Double>{
+        return temp3
+    }
+    fun getTemp4():MutableLiveData<Double>{
+        return temp4
+    }
+    fun getTemp5():MutableLiveData<Double>{
+        return temp5
+    }
+
+
+    fun getDate1():MutableLiveData<String>{
+        return date1
+    }
+    fun getDate2():MutableLiveData<String>{
+        return date2
+    }
+    fun getDate3():MutableLiveData<String>{
+        return date3
+    }
+    fun getDate4():MutableLiveData<String>{
+        return date4
+    }
+    fun getDate5():MutableLiveData<String>{
+        return date5
     }
 
     fun getDate():MutableLiveData<String>{
@@ -43,13 +86,13 @@ class SecondViewModel : ViewModel() {
         return feelsLike
     }
 
-//    fun getMinTemp():MutableLiveData<Double>{
-//        return minTemp
-//    }
-//
-//    fun getMaxTemp():MutableLiveData<Double>{
-//        return maxTemp
-//    }
+    fun getMinTemp():MutableLiveData<Double>{
+        return minTemp
+    }
+
+    fun getMaxTemp():MutableLiveData<Double>{
+        return maxTemp
+    }
 
     fun getMainDescription():MutableLiveData<String>{
         return mainDescription
@@ -75,8 +118,8 @@ class SecondViewModel : ViewModel() {
             wind.setValue(windSpeed.getDouble("speed"))
             humidity.setValue(main.getInt("humidity"))
             feelsLike.setValue(main.getDouble("feels_like"))
-//            minTemp.setValue(main.getDouble("temp_min"))
-//            maxTemp.setValue(main.getDouble("temp_max"))
+            minTemp.setValue(main.getDouble("temp_min"))
+            maxTemp.setValue(main.getDouble("temp_max"))
             mainDescription.setValue(description.getString("main"))
 
 
@@ -84,6 +127,49 @@ class SecondViewModel : ViewModel() {
         },Response.ErrorListener { temp.setValue(0.0) }
         )
         queue.add(stringReq)
+    }
+    fun forecast(city: String, queue: RequestQueue){
+        val url = "https://api.openweathermap.org/data/2.5/forecast?q=$city&units=imperial&appid=$API"
+
+        val stringReq2 = StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
+
+            val obj = JSONObject(response)
+            val myList0 = obj.getJSONArray("list").getJSONObject(0)
+            val main0 = myList0.getJSONObject("main")
+            temp1.setValue(main0.getDouble("temp"))
+            val toDate0 = myList0.getLong("dt")
+            date1.setValue(SimpleDateFormat("EEEE, MMMM dd", Locale.ENGLISH).format(Date(toDate0 * 1000)))
+
+
+            val myList8 = obj.getJSONArray("list").getJSONObject(8)
+            val main8 = myList8.getJSONObject("main")
+            temp2.setValue(main8.getDouble("temp"))
+            val toDate8 = myList8.getLong("dt")
+            date2.setValue(SimpleDateFormat("EEEE, MMMM dd", Locale.ENGLISH).format(Date(toDate8 * 1000)))
+
+            val myList16 = obj.getJSONArray("list").getJSONObject(16)
+            val main16 = myList16.getJSONObject("main")
+            temp3.setValue(main16.getDouble("temp"))
+            val toDate16 = myList16.getLong("dt")
+            date3.setValue(SimpleDateFormat("EEEE, MMMM dd", Locale.ENGLISH).format(Date(toDate16 * 1000)))
+
+            val myList24 = obj.getJSONArray("list").getJSONObject(24)
+            val main24 = myList24.getJSONObject("main")
+            temp4.setValue(main24.getDouble("temp"))
+            val toDate24 = myList24.getLong("dt")
+            date4.setValue(SimpleDateFormat("EEEE, MMMM dd", Locale.ENGLISH).format(Date(toDate24 * 1000)))
+
+            val myList32 = obj.getJSONArray("list").getJSONObject(32)
+            val main32 = myList32.getJSONObject("main")
+            temp5.setValue(main32.getDouble("temp"))
+            val toDate32 = myList32.getLong("dt")
+            date5.setValue(SimpleDateFormat("EEEE, MMMM dd", Locale.ENGLISH).format(Date(toDate32 * 1000)))
+
+        },
+            Response.ErrorListener { temp.setValue(0.0) }
+        )
+
+        queue.add(stringReq2)
     }
 
 }
